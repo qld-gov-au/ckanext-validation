77e69a22e2ca6d296238e01e7e1469428d8be9a5
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-09-08 15:03:35 +1000

    [QOL-8251] fix curl command
    - refactoring to environment variable didn't work

--- .docker/scripts/create-test-data.sh
+++ .docker/scripts/create-test-data.sh
@@ -21,7 +21,6 @@ add_user_if_needed () {
 
 # We know the "admin" sysadmin account exists, so we'll use her API KEY to create further data
 API_KEY=$(ckan_cli user admin | tr -d '\n' | sed -r 's/^(.*)apikey=(\S*)(.*)/\2/')
-CURL="curl -L -s --header 'Authorization: ${API_KEY}'"
 
 # Creating test data hierarchy which creates organisations assigned to datasets
 ckan_cli create-test-data hierarchy
@@ -37,49 +36,56 @@ add_user_if_needed walker "Walker" walker@localhost
 
 echo "Updating annakarenina to use department-of-health Organisation:"
 package_owner_org_update=$( \
-    $CURL --data "id=annakarenina&organization_id=department-of-health" \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=annakarenina&organization_id=department-of-health" \
     ${CKAN_ACTION_URL}/package_owner_org_update
 )
 echo ${package_owner_org_update}
 
 echo "Updating organisation_admin to have admin privileges in the department-of-health Organisation:"
 organisation_admin_update=$( \
-    $CURL --data "id=department-of-health&username=organisation_admin&role=admin" \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=department-of-health&username=organisation_admin&role=admin" \
     ${CKAN_ACTION_URL}/organization_member_create
 )
 echo ${organisation_admin_update}
 
 echo "Updating publisher to have editor privileges in the department-of-health Organisation:"
 publisher_update=$( \
-    $CURL --data "id=department-of-health&username=publisher&role=editor" \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=department-of-health&username=publisher&role=editor" \
     ${CKAN_ACTION_URL}/organization_member_create
 )
 echo ${publisher_update}
 
 echo "Updating foodie to have admin privileges in the food-standards-agency Organisation:"
 foodie_update=$( \
-    $CURL --data "id=food-standards-agency&username=foodie&role=admin" \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=food-standards-agency&username=foodie&role=admin" \
     ${CKAN_ACTION_URL}/organization_member_create
 )
 echo ${foodie_update}
 
 echo "Creating non-organisation group:"
 group_create=$( \
-    $CURL --data "name=silly-walks" \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "name=silly-walks" \
     ${CKAN_ACTION_URL}/group_create
 )
 echo ${group_create}
 
 echo "Updating group_admin to have admin privileges in the silly-walks group:"
 group_admin_update=$( \
-    $CURL --data "id=silly-walks&username=group_admin&role=admin" \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=silly-walks&username=group_admin&role=admin" \
     ${CKAN_ACTION_URL}/group_member_create
 )
 echo ${group_admin_update}
 
 echo "Updating walker to have editor privileges in the silly-walks group:"
 walker_update=$( \
-    $CURL --data "id=silly-walks&username=walker&role=editor" \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=silly-walks&username=walker&role=editor" \
     ${CKAN_ACTION_URL}/group_member_create
 )
 echo ${walker_update}
