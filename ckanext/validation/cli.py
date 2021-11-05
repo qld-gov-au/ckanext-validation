f210171a444cfaf2470f86d7681d519b4a4d4067
duskobogdanovski <dusko.bogdanovski@keitaro.com>
2021-11-05 17:28:30 +0100

    Make CLI backward compatible

--- ckanext/validation/cli.py
+++ ckanext/validation/cli.py
@@ -1,5 +1,6 @@
 import click
 
+import ckanext.validation.common as common
 
 def get_commands():
     return [validation]
@@ -14,5 +15,43 @@ def validation():
 
 @validation.command(name='init-db')
 def init_db():
-    from ckanext.validation.common import init_db
-    init_db()
+    common.init_db()
+
+
+@validation.command(name='run')
+@click.option(u'-y', u'--yes',
+              help=u'Automatic yes to prompts. Assume "yes" as answer '
+                   u'to all prompts and run non-interactively',
+              default=False)
+@click.option('-r', '--resource',
+              multiple=True,
+              help=u'Run data validation on a particular resource (if the format is suitable).'
+                   u'It can be defined multiple times. Not to be used with -d or -s')
+@click.option('-d', '--dataset',
+              multiple=True,
+              help=u'Run data validation on all resources for a particular dataset (if the format is suitable).'
+                   u' You can use the dataset id or name, and it can be defined multiple times. '
+                   u'Not to be used with -r or -s')
+@click.option('-s', '--search',
+              default=False,
+              help=u'Run data validation on all resources for a particular dataset (if the format is suitable).'
+                   u' You can use the dataset id or name, and it can be defined multiple times. '
+                   u'Not to be used with -r or -s')
+def run_validation(yes, resource, dataset, search):
+    common.run_validation(yes, resource, dataset, search)
+
+
+@validation.command()
+@click.option(u'-o', u'--output',
+              help=u'Location of the CSV validation report file on the relevant commands.',
+              default=u'validation_errors_report.csv')
+def report(output):
+    common.report(output)
+
+
+@validation.command(name='report-full')
+@click.option(u'-o', u'--output',
+              help=u'Location of the CSV validation report file on the relevant commands.',
+              default=u'validation_errors_report.csv')
+def report_full(output):
+    common.report(output)
--- ckanext/validation/logic.py
+++ ckanext/validation/logic.py
@@ -262,14 +262,14 @@ def resource_validation_run_batch(context, data_dict):
     count_resources = 0
 
     dataset_ids = data_dict.get('dataset_ids')
-    if isinstance(dataset_ids, basestring):
+    if isinstance(dataset_ids, str):
         try:
             dataset_ids = json.loads(dataset_ids)
         except ValueError as e:
             dataset_ids = [dataset_ids]
 
     search_params = data_dict.get('query')
-    if isinstance(search_params, basestring):
+    if isinstance(search_params, str):
         try:
             search_params = json.loads(search_params)
         except ValueError as e:
