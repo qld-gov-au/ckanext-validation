dec07469a8808ae7e6d2d2b9c23dec6fd9bb3731
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-02-15 09:12:07 +1000

    [QOL-7740] adjust warning message according to CKAN version

--- ckanext/validation/plugin.py
+++ ckanext/validation/plugin.py
@@ -60,14 +60,15 @@ class ValidationPlugin(MixinPlugin, p.SingletonPlugin, DefaultTranslation):
 
     def update_config(self, config_):
         if not tables_exist():
+            if toolkit.check_ckan_version('2.9'):
+                init_command = 'ckan validation init-db'
+            else:
+                init_command = 'paster --plugin=ckanext-validation validation init-db'
             log.critical(u'''
 The validation extension requires a database setup.
 Validation pages will not be enabled.
 Please run the following to create the database tables:
-    paster --plugin=ckanext-validation validation init-db
-OR
-    ckan validation init-db
-''')
+    %s''', init_command)
         else:
             log.debug(u'Validation tables exist')
 
