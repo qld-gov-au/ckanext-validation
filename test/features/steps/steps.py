3c6c118d130e965e77b083026c8bc0a28ec46e88
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-02-24 13:52:03 +1000

    [QOL-7740] add preposition to Behave step definition

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -112,6 +112,6 @@ def go_to_organisation_including_users(context, organisation_id, including):
 @step(u'I should see a validation timestamp')
 def should_see_validation_timestamp(context):
     context.execute_steps(u"""
-        I should see "Validation timestamp"
+        Then I should see "Validation timestamp"
         And I should see an element with xpath "//th[contains(string(), 'Validation timestamp')]/../td[contains(string(), '-') and contains(string(), 'T') and contains(string(), ':') and contains(string(), '.')]"
     """)
