8d29362c9168f4c6c139dc3d484ea6ac30ee639f
Hristijan Vilos <hristijan.vilos@protonmail.com>
2022-07-01 13:56:09 +0200

    Fix and migrate helpers tests

--- ckanext/validation/tests/test_helpers.py
+++ ckanext/validation/tests/test_helpers.py
@@ -1,37 +1,25 @@
 import datetime
 
-from nose.tools import assert_equals, assert_in
+import pytest
 
-from ckan.tests.helpers import reset_db
 from ckan.tests import factories
-
-from ckantoolkit import config
 
 from ckanext.validation.helpers import (
     get_validation_badge,
     validation_extract_report_from_errors,
 )
-from ckanext.validation.model import create_tables, tables_exist
 
 
+def _assert_validation_badge_status(resource, status):
+    out = get_validation_badge(resource)
+
+    assert 'href="/dataset/{}/resource/{}/validation"'.format(
+        resource['package_id'], resource['id']) in out
+    assert 'class="status {}"'.format(status) in out
+
+
+@pytest.mark.usefixtures("clean_db", "validation_setup")
 class TestBadges(object):
-
-    @classmethod
-    def setup_class(cls):
-        cls._original_config = dict(config)
-        config['ckanext.validation.run_on_create_sync'] = False
-
-        reset_db()
-        if not tables_exist():
-            create_tables()
-
-    @classmethod
-    def teardown_class(cls):
-
-        config.clear()
-        config.update(cls._original_config)
-
-        reset_db()
 
     def test_get_validation_badge_no_validation(self):
 
@@ -39,55 +27,37 @@ class TestBadges(object):
             format='CSV',
         )
 
-        assert_equals(get_validation_badge(resource), '')
+        assert get_validation_badge(resource) == ''
 
     def test_get_validation_badge_success(self):
 
         resource = factories.Resource(
             format='CSV',
             validation_status='success',
-            validation_timestamp=datetime.datetime.utcnow()
+            validation_timestamp=datetime.datetime.utcnow().isoformat()
         )
 
-        out = get_validation_badge(resource)
-
-        assert 'href="/dataset/{}/resource/{}/validation"'.format(
-            resource['package_id'], resource['id']) in out
-        assert 'src="/images/badges/data-success-flat.svg"' in out
-        assert 'alt="Valid data"' in out
-        assert 'title="{}"'.format(resource['validation_timestamp']) in out
+        _assert_validation_badge_status(resource, 'success')
 
     def test_get_validation_badge_failure(self):
 
         resource = factories.Resource(
             format='CSV',
             validation_status='failure',
-            validation_timestamp=datetime.datetime.utcnow()
+            validation_timestamp=datetime.datetime.utcnow().isoformat()
         )
 
-        out = get_validation_badge(resource)
-
-        assert 'href="/dataset/{}/resource/{}/validation"'.format(
-            resource['package_id'], resource['id']) in out
-        assert 'src="/images/badges/data-failure-flat.svg"' in out
-        assert 'alt="Invalid data"' in out
-        assert 'title="{}"'.format(resource['validation_timestamp']) in out
+        _assert_validation_badge_status(resource, 'invalid')
 
     def test_get_validation_badge_error(self):
 
         resource = factories.Resource(
             format='CSV',
             validation_status='error',
-            validation_timestamp=datetime.datetime.utcnow()
+            validation_timestamp=datetime.datetime.utcnow().isoformat()
         )
 
-        out = get_validation_badge(resource)
-
-        assert 'href="/dataset/{}/resource/{}/validation"'.format(
-            resource['package_id'], resource['id']) in out
-        assert 'src="/images/badges/data-error-flat.svg"' in out
-        assert 'alt="Error during validation"' in out
-        assert 'title="{}"'.format(resource['validation_timestamp']) in out
+        _assert_validation_badge_status(resource, 'error')
 
     def test_get_validation_badge_other(self):
 
@@ -96,13 +66,7 @@ class TestBadges(object):
             validation_status='not-sure',
         )
 
-        out = get_validation_badge(resource)
-
-        assert 'href="/dataset/{}/resource/{}/validation"'.format(
-            resource['package_id'], resource['id']) in out
-        assert 'src="/images/badges/data-unknown-flat.svg"' in out
-        assert 'alt="Data validation unknown"' in out
-        assert 'title=""' in out
+        _assert_validation_badge_status(resource, 'unknown')
 
 
 class TestExtractReportFromErrors(object):
@@ -122,12 +86,12 @@ class TestExtractReportFromErrors(object):
         extracted_report, errors = validation_extract_report_from_errors(
             errors)
 
-        assert_equals(extracted_report, report)
-        assert_equals(errors['some_field'], ['Some error'])
+        assert extracted_report == report
+        assert errors['some_field'] == ['Some error']
         assert str(errors['validation'][0]).strip().startswith(
             'There are validation issues with this file')
 
-        assert_in('data-module="modal-dialog"', str(errors['validation'][0]))
+        assert 'data-module="modal-dialog"' in str(errors['validation'][0])
 
     def test_report_not_extracted(self):
 
@@ -139,5 +103,5 @@ class TestExtractReportFromErrors(object):
         report, errors = validation_extract_report_from_errors(errors)
 
         assert report is None
-        assert_equals(errors['some_field'], ['Some error'])
-        assert_equals(errors['some_other_field'], ['Some other error'])
+        assert errors['some_field'] == ['Some error']
+        assert errors['some_other_field'] == ['Some other error']
