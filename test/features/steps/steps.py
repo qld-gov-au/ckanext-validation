7959368160df6c821f9973545b1368182af41252
antuarc <carl.antuar@smartservice.qld.gov.au>
2023-04-28 10:36:12 +1000

    [QOLDEV-323] sync scenario test steps with ckanext-qgov

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -53,8 +53,7 @@ def attempt_login(context, password):
 @step(u'I open the new resource form for dataset "{name}"')
 def go_to_new_resource_form(context, name):
     context.execute_steps(u"""
-        When I go to dataset "{name}"
-        And I click the link with text that contains "Manage"
+        When I edit the "{name}" dataset
         And I click the link with text that contains "Resources"
         And I click the link with text that contains "Add new resource"
     """.format(name=name))
@@ -65,11 +64,12 @@ def add_resource(context, name, url):
     context.execute_steps(u"""
         When I log in
         And I open the new resource form for dataset "warandpeace"
-        And I press the element with xpath "//form[@id='resource-edit']//a[string() = 'Link']"
-        And I fill in "name" with "{}"
-        And I fill in "url" with "{}"
-        And I press the element with xpath "//button[contains(string(), 'Add')]"
-    """.format(name, url))
+        And I execute the script "$('#resource-edit [name=url]').val('{url}')"
+        And I fill in "name" with "{name}"
+        And I fill in "description" with "description"
+        And I execute the script "document.getElementById('field-format').value='HTML'"
+        And I press the element with xpath "//form[contains(@class, 'resource-form')]//button[contains(@class, 'btn-primary')]"
+    """.format(name=name, url=url))
 
 
 @step(u'I go to dataset page')
@@ -80,6 +80,14 @@ def go_to_dataset_page(context):
 @step(u'I go to dataset "{name}"')
 def go_to_dataset(context, name):
     when_i_visit_url(context, '/dataset/' + name)
+
+
+@step(u'I edit the "{name}" dataset')
+def edit_dataset(context, name):
+    context.execute_steps(u"""
+        When I go to dataset "{name}"
+        And I click the link with text that contains "Manage"
+    """.format(name=name))
 
 
 @step(u'I go to organisation page')
@@ -109,7 +117,16 @@ def go_to_user_profile(context, user_id):
 
 @step(u'I go to the dashboard')
 def go_to_dashboard(context):
-    when_i_visit_url(context, '/dashboard')
+    context.execute_steps(u"""
+        When I visit "/dashboard/datasets"
+    """)
+
+
+@step(u'I should see my datasets')
+def dashboard_datasets(context):
+    context.execute_steps(u"""
+        Then I should see an element with xpath "//li[contains(@class, 'active') and contains(string(), 'My Datasets')]"
+    """)
 
 
 @step(u'I go to the "{user_id}" user API')
@@ -119,12 +136,19 @@ def go_to_user_show(context, user_id):
 
 @step(u'I view the "{group_id}" group API "{including}" users')
 def go_to_group_including_users(context, group_id, including):
-    when_i_visit_url(context, r'/api/3/action/group_show?id={}&include_users={}'.format(group_id, including in ['with', 'including']))
+    when_i_visit_url(
+        context, r'/api/3/action/group_show?id={}&include_users={}'.format(
+            group_id, including in ['with', 'including']))
 
 
 @step(u'I view the "{organisation_id}" organisation API "{including}" users')
 def go_to_organisation_including_users(context, organisation_id, including):
-    when_i_visit_url(context, r'/api/3/action/organization_show?id={}&include_users={}'.format(organisation_id, including in ['with', 'including']))
+    when_i_visit_url(
+        context, r'/api/3/action/organization_show?id={}&include_users={}'.format(
+            organisation_id, including in ['with', 'including']))
+
+
+# ckanext-validation
 
 
 @step(u'I should see a validation timestamp')
