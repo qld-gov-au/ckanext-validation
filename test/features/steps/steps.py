b980717e20efabaa34a34f9df315f801fdeb7608
william dutton <will.dutt@gmail.com>
2025-01-04 10:38:32 +1000

    fix wip

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -103,7 +103,7 @@ def go_to_new_resource_form(context, name):
     elif context.browser.is_element_present_by_xpath("//*[contains(string(), 'Add new resource')]"):
         # Existing dataset, browse to the resource form
         context.execute_steps(u"""
-                   And I press "Add new resource"
+                   When I press "Add new resource"
                """)
     else:
         # Existing dataset, browse to the resource form
